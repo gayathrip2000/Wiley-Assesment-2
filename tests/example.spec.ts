4c8c7b7a2505abd74dc29fba8725c0ab32aecbc8
Gayathri Gamage <99193625+gayathrip2000@users.noreply.github.com>
2024-01-06 12:31:44 +0530

    verify the search functionality

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,41 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+//crate constant variable for search box
+const BOOK_ISBN = "9783527610853";
+const BOOK_NAME = "100 Years Werner Heisenberg"
+const ABOUT_THIS_BOOK = "About this Book";
+
+const SEARCH_BOX_INPUT = '//*[@id="searchField1"]';
+const SEARCH_RESULT_TEXT1 = '//span[@class=\'result__count\']\n'
+const SEARCH_RESULT_TEXT2 = '//span[@class=\'result__sep\']\n'
+const SEARCH_RESULT_TEXT3 = '//span[@class=\'result__suffix\']\n'
+const PRODUCT_TITLE = '//span[@class=\'hlFld-Title\']'
+
+  test('Verify the search functionality of Wiley Online Library', async ({ page }) => {
+  await page.goto('https://onlinelibrary.wiley.com/');
+
+  //verify the title
+  await expect(page).toHaveTitle("Wiley Online Library | Scientific research articles, journals, books, and reference works",{timeout:10000});
+
+  //input the book isbn in the search box
+  await page.locator(SEARCH_BOX_INPUT).pressSequentially(BOOK_ISBN);
+
+  await page.keyboard.press('Enter');
+
+  //verify the search result for 9783527610853
+  await expect(page.locator(SEARCH_RESULT_TEXT1)).toHaveText(`1`);
+  await expect(page.locator(SEARCH_RESULT_TEXT2)).toHaveText(`results for`);
+  await expect(page.locator(SEARCH_RESULT_TEXT3)).toHaveText(`"${BOOK_ISBN}"  anywhere `);
+
+  //verify the book name
+  await expect(page.locator(PRODUCT_TITLE).first()).toHaveText(BOOK_NAME,{timeout :5000});
+  // Navigate and click element
+
+  await page.getByText(ABOUT_THIS_BOOK).click();
+  await page.waitForURL('**/action/doSearch?AllField=9783527610853#');
+
+
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});
